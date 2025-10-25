621601ba2c1519d4ebe6c65b768b663e9740f842
chrisrossell <chrisrossell1999@gmail.com>
2025-10-25 09:26:52 +0100

    Remove todo

--- app/Homepage.jsx
+++ app/Homepage.jsx
@@ -36,7 +36,6 @@ const App = () => {
     );
 };
 
-// TODO: fix this styling so it actually uses css (is this using tailwind.css?)
 const Header = ({ onNavClick, activeSection }) => (
     <header className="bg-white shadow-md sticky top-0 z-10">
         <div className="container mx-auto px-4 py-4 flex justify-between items-center max-w-6xl">
